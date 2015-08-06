4e1b94d83c860040c07bff0b048e8cbd53e90ea3
b-stone <bstone@bard.edu>
2015-08-06 12:13:43 -0400

    ruby test removes \item and other stuff

--- tools/use_tex_input.rb
+++ tools/use_tex_input.rb
@@ -7,6 +7,31 @@ ARGF.each do |line|
   	text = File.read("book/"+$1+ ".tex")
     line.gsub!(line, text )
   end
+
+  if line =~ /\\framebox\{\\hspace\*\{3 pt\}/ 
+    line.gsub!(line, "\\{\\% include motivating-quest-top.md \\%\\}" )
+  end
+
+  if line =~ /\\parbox\{\\boxwidth\}\{\\begin\{goals\}/
+    line.gsub!(line, "\\begin{itemize}" )
+  end   
+
+  if line =~ /\\end\{goals\}\} \\hspace\*\{3 pt\}\}/
+    line.gsub!(line, "\\end{itemize}\n\\{\\% include motivating-quest-bot.md \\%\\}" )    
+  end
+
+  if line["\\ba"]
+  	line.gsub!(line["\\ba"], "\\begin{enumerate}" )
+  end   
+
+  if line["\\ea"]
+  	line.gsub!(line["\\ea"], "\\end{enumerate}" )
+  end     
+
+#  if line["\\item"]
+#    line.gsub!( line["\\item"], "* ")
+#  end  
+
   puts line
 end
 
